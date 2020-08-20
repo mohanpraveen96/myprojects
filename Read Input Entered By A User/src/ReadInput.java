e083acccae3b7815e4410f73168fd22c89b05217
mohanpraveen96 <69713458+mohanpraveen96@users.noreply.github.com>
2020-08-20 13:26:01 +0530

    Add files via upload

---
+++ Read Input Entered By A User/src/ReadInput.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ReadInput {
+
+	public static void main(String[] args) {
+		
+		Scanner scan=new Scanner(System.in);
+		System.out.println("enter a number: ");
+		
+		int number=scan.nextInt();
+		System.out.println("You entered: "+ number);
+	}
+
+}
