0d4884ae1bc1dcdd97575b9bd9ad237a0efa819d
mohanpraveen96 <69713458+mohanpraveen96@users.noreply.github.com>
2020-08-23 10:06:50 +0530

    Add files via upload

---
+++ swapping two numbers/src/Swap.java
@@ -0,0 +1,18 @@
+
+public class Swap {
+
+	public static void main(String[] args) {
+		int x=10;
+		int y=18;
+		int temp;
+		
+		temp=x;
+		x=y;
+		y=temp;
+		
+		System.out.println(" x: "+x);
+		System.out.println(" y: "+y);
+
+	}
+
+}
